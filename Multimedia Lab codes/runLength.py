3f62b1ac7445052ff4c09e85b5cb0f9f0591c3dd
Raunit <36741947+raunit-x@users.noreply.github.com>
2019-10-24 13:24:02 +0530

    Update runLength.py

--- Multimedia Lab codes/runLength.py
+++ Multimedia Lab codes/runLength.py
@@ -1,21 +1,14 @@
-def run_length_compression(input: str) -> str:
-    result = ''
-    for c in input:
-        if not result.count(c):
-            result += c + str(input.count(c))
-    return result
+def run_length_encoding(input_str: str) -> str:
+    return ''.join(c + str(input_str.count(c)) for c in set(input_str))
 
 
-def run_length_expansion(compressed_text: str) -> str:
-    result = ''
-    for i in range(0, len(compressed_text), 2):
-        result += "".join(compressed_text[i] for j in range(int(compressed_text[i + 1])))
-    return result
+def run_length_decoding(encoded_text: str) -> str:
+    return "".join([encoded_text[i] * int(encoded_text[i + 1]) for i in range(0, len(encoded_text), 2)])
 
 
-input_text = input("Enter String: ")
-compressed_text = run_length_compression(input_text)
-print("Compressed string using run length encoding: {}".format(compressed_text))
-expanded_text = run_length_expansion(compressed_text)
-print("Compressed string using run length encoding: {}".format(expanded_text))
-
+if __name__ == '__main__':
+    input_text = input("Enter String: ")
+    encoded_text = run_length_encoding(input_text)
+    print("Compressed string using run length encoding: {}".format(encoded_text))
+    expanded_text = run_length_decoding(encoded_text)
+    print("Compressed string using run length encoding: {}".format(expanded_text))
